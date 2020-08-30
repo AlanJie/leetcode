99d7bfeaba6d833a3479257ff94aab2c52c9cf03
AlanJie <allenjie95@gmail.com>
2020-08-30 08:04:58 +0800

    557 add another solution

--- simple/557.py
+++ simple/557.py
@@ -1,11 +1,14 @@
 class Solution:
+    # def reverseWords(self, s: str) -> str:
+    #     str_list = s.split(' ')
+    #     str_list = [s[::-1] for s in str_list]
+
+    #     s = ' '.join(str_list)
+
+    #     return s
+
     def reverseWords(self, s: str) -> str:
-        str_list = s.split(' ')
-        str_list = [s[::-1] for s in str_list]
-
-        s = ' '.join(str_list)
-
-        return s
+        return ' '.join(x[::-1] for x in s.split(' '))
 
 
 if __name__ == '__main__':
